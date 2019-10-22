d54530e38ae0df87c4bf4942bd38ce28bac4937c
unknown <tcutlip08@gmail.com>
2019-10-22 17:46:23 -0400

    firebase works and on successful creation of account add data to mongoDB

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import config from "./firebase";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import NavBar from "./components/NavBar";
 
-
-firebase.initializeApp(config);
-
-
-class App extends Component {
-  state = {
-    user: ""
-  };
-
-  componentDidMount() {
-    const auth = firebase.auth();
-
-    // const promise = auth.createUserWithEmailAndPassword(
-    //   "tcutlip08@gmail.com",
-    //   "wizkik101"
-    // );
-
-    // promise
-    //   .then(result => {
-    //     console.log(result);
-    //   })
-    //   .catch(e => {
-    //     console.log(e.message);
-    //   });
-
-    auth.onAuthStateChanged(function(fbUser) {
-      if (fbUser) {
-        console.log(fbUser.email);
-      }
-    });
-  }
-  render() {
-    return (
-      <Router>
-        <>
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={Landing} />
-          </Switch>
-        </>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </>
+    </Router>
+  );
 }
 
 export default App;
