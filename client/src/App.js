ff341327465b20aba095c4a0d4b96c176ab7a116
tcutlip08 <48369513+tcutlip08@users.noreply.github.com>
2019-10-23 19:25:18 -0400

    Update App.js
    Trying to keep it camel case to make naming uniform

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import NavBar from "./components/NavBar";
 import ParentDashboard from "./pages/ParentDashboard";
 
@@ -11,7 +11,7 @@ function App() {
       <>
         <NavBar />
         <Switch>
-          <Route exact path="/sign-up" component={Signup} />
+          <Route exact path="/sign-up" component={SignUp} />
           <Route exact path="/parent" component={ParentDashboard}/>
           <Route exact path="/" component={Landing} />
         </Switch>
