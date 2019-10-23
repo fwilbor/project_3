b16e56ddf18e8c62c3bd2d5a159b2f15e463e289
Jesselyn Jackson <Jesselyn.Jackson@gmail.com>
2019-10-23 18:57:26 -0400

    Initial layout for parent dashboard.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 import NavBar from "./components/NavBar";
+import ParentDashboard from "./pages/ParentDashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/sign-up" component={Signup} />
+          <Route exact path="/parent" component={ParentDashboard}/>
           <Route exact path="/" component={Landing} />
         </Switch>
       </>
