3ee96bb7fd82ac3d670ccd5cd04dc6d6b36185ea
Jesse McKinney <jessman51386@gmail.com>
2019-10-25 15:05:12 -0400

    i got rid of all of the extra react files that we didn't need

--- client/src/pages/MathGame/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MathGame from './MathGame';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MathGame />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
