876e84377447fc11d962d094a7ce6b0734812e42
fwilbor <franklin.cfg@gmail.com>
2019-10-26 17:12:05 -0400

    update Stats Icon

--- client/src/pages/ChildDashboard.js
+++ client/src/pages/ChildDashboard.js
@@ -55,72 +55,72 @@ class ChildDashboard extends Component {
               </div>
             </div>
             <div className="col-md-8">
-            <div className="row">
-            <h3>Pick A Game!</h3>
-              <div className="bd-example">
-            <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-                </ol>
-                <div className="carousel-inner">
-                <Link to="/mathGame" className="carousel-item active">
-                    <img src="../assets/img/bg.jpg" className="carouselImg" alt="game"/>
-                    <div className="carousel-caption d-none d-md-block">
-                    <h5>Math Game</h5>
-                    <p>Are you an arithmetic boss?</p>
-                    </div>
-                </Link>
-                <div className="carousel-item ">
-                    <img src="../assets/img/bg0.jpg" className="carouselImg" alt="game"/>
-                    <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                <img src="../assets/img/dg2.jpg" className="carouselImg" alt="game"/>
-                    <div className="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                <img src="../assets/img/dg6.jpg" className="carouselImg" alt="game"/>
-                    <div className="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </div>
-                </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-                </a>
-            </div>
-            </div>
-            </div>
-            <div className="row">
               <div className="row">
-                <div className="info info-horizontal">
-                  <div className="icon icon-primary">
-                    <i className="material-icons">extension</i>
+                <h3>Pick A Game!</h3>
+                <div className="bd-example">
+                  <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                      <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                      <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+                    </ol>
+                    <div className="carousel-inner">
+                      <Link to="/mathGame" className="carousel-item active">
+                        <img src="../assets/img/bg.jpg" className="carouselImg" alt="game" />
+                        <div className="carousel-caption d-none d-md-block">
+                          <h5>Math Game</h5>
+                          <p>Are you an arithmetic boss?</p>
+                        </div>
+                      </Link>
+                      <div className="carousel-item ">
+                        <img src="../assets/img/bg0.jpg" className="carouselImg" alt="game" />
+                        <div className="carousel-caption d-none d-md-block">
+                          <h5>First slide label</h5>
+                          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </div>
+                      </div>
+                      <div className="carousel-item">
+                        <img src="../assets/img/dg2.jpg" className="carouselImg" alt="game" />
+                        <div className="carousel-caption d-none d-md-block">
+                          <h5>Second slide label</h5>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                      </div>
+                      <div className="carousel-item">
+                        <img src="../assets/img/dg6.jpg" className="carouselImg" alt="game" />
+                        <div className="carousel-caption d-none d-md-block">
+                          <h5>Third slide label</h5>
+                          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span className="sr-only">Next</span>
+                    </a>
                   </div>
-                  <div className="description">
-                    <h4 className="info-title">Stats</h4>
-                    <p>Check your gaming history and stats.</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="row">
+                  <div className="info info-horizontal">
+                    <div className="icon icon-primary">
+                      <i className="material-icons">poll</i>
+                    </div>
+                    <div className="description">
+                      <h4 className="info-title">Stats</h4>
+                      <p>Check your gaming history and stats.</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </>
     );
