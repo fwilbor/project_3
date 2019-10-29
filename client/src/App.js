5a16218b2beaacebc4a440b3c2584b63d4c57f71
unknown <tcutlip08@gmail.com>
2019-10-29 17:40:51 -0400

    commiting to pull

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import COPPA from "./pages/COPPA";
 import ParentDashboard from "./pages/ParentDashboard";
 import ChildDashboard from "./pages/ChildDashboard";
 import MathQuiz from "./components/Games/MathQuiz/MathQuiz";
-import AddQuiz from "./components/Games/MathQuiz/AddQuiz";
-import SubQuiz from "./components/Games/MathQuiz/SubractQuiz";
-import MultiQuiz from "./components/Games/MathQuiz/MultiplyQuiz";
-import DivQuiz from "./components/Games/MathQuiz/DivideQuiz";
+import AddQuiz from "./components/Games/AddQuiz/AddQuiz";
+import SubQuiz from "./components/Games/SubtractQuiz/SubtractQuiz";
+import MultiQuiz from "./components/Games/MultiplyQuiz/MultiplyQuiz";
+import DivQuiz from "./components/Games/DivideQuiz/DivideQuiz";
 import About from "./components/Menu/About";
 import Philosophy from "./components/Menu/Philosophy";
 
@@ -29,6 +29,7 @@ function App() {
         <Route exact path="/:id/multiQuiz" component={MultiQuiz} />
         <Route exact path="/:id/divQuiz" component={DivQuiz} />
         <Route exact path="/" component={Landing} />
+        <Route exact path="*" component={Error404} />
       </Switch>
     </Router>
   );
