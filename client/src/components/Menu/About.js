1779eb431b1ab9e41630523963dfe59063698f15
fwilbor <franklin.cfg@gmail.com>
2019-10-30 00:52:30 -0400

    grammar updates to About page

--- client/src/components/Menu/About.js
+++ client/src/components/Menu/About.js
@@ -10,15 +10,13 @@ function About() {
         <p>
         J Bot's vision is to build the must-have-app for kids to LEARN, HAVE FUN, and STAY SAFE in an increasingly complex digital and social media age.
 
-        Designed to supervise, manage, and protect your child’s learning in a Monitored Online Learning Environment (M.O.L.E)
+        Designed to supervise, manage, and protect your child’s learning in a Monitored Online Learning Environment (M.O.L.E); the J-Bot application is a COPPA (Child Online Privacy Protection Act) compliant learning application for kids under the age of 16. 
         
-        J-Bot App is a COPPA (Child Online Privacy Protection Act) compliant Learning application for kids under the age of 16. 
+        J-Bot is easy to setup. Parents and guardians can get their children started on their educational journey within minutes.
         
-        J-Bot is easy to setup. Parents and Guardians can get their children started on their educational journey within minutes.
+        Stay connected with your child’s progress with J-Bot's up-to-date progress summary in your Parental Dashboard.
         
-        Stay connected with your child’s progress with J-Bot's up to date progress summary in your Parental Dashboard
-        
-        Get instant notifications on improvements, skill completions and STAY INVOLVED!
+      
         
         </p>
       </div>
