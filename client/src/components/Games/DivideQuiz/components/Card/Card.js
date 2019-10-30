c606a92c880869a3fb6eae3e5b68153f2cdd5d85
Jesse McKinney <jessman51386@gmail.com>
2019-10-30 12:21:20 -0400

    worked a little on the games and fixed the cards. the props.questions were screwed up from the new react and bootstrap and react strap stuff but, I got it all aligned again, Should have no merge conflicts. I only worked on the things I was working on last night

--- client/src/components/Games/DivideQuiz/components/Card/Card.js
+++ client/src/components/Games/DivideQuiz/components/Card/Card.js
@@ -6,7 +6,7 @@ const Card = props => {
   return (
     <div className="container">
       <div id="dividecard" className="card" clicked={props.clicked}>
-        <div className="row m-auto pt-2"> {props.question}</div>
+        <div className="row m-auto pt-2" id="propsQuestions"> {props.question}</div>
         <div className="row"><Button answers={props.answers} correctAnswer={props.correctAnswer} testClick={props.testClick} /></div>
       </div>
     </div>
